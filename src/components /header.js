c88a528d1bd7b8fd56b4ce81a14029963c12f78f
azizou98 <abdelaziz.aitmoussa@etu.usthb.dz>
2024-03-03 15:26:55 +0100

    adding component

---
+++ src/components /header.js
@@ -0,0 +1,15 @@
+// Header.js
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>AIT MOUSSA ABDELAZIZ</h1>
+      <p>Web Developer</p>
+      {/* Add navigation links if needed */}
+    </header>
+  );
+};
+
+export default Header;
