f8ad06b680485f4381b611b0f162357cb388acfe
azizou98 <abdelaziz.aitmoussa@etu.usthb.dz>
2024-03-03 16:19:59 +0100

    adding compon with cap suc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components /Header';
 // import AboutMe from './components/AboutMe';  // Import other components similarly
 //import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
+import Skills from './components /Skills';
+import Contact from './components /Contact';
 
 function App() {
   return (
